3ca0e6ba63992d029a72347c9514c7596c73feba
Paraic Mc Donagh <S00188844@mail.itsligo.ie>
2020-05-13 11:04:25 +0100

    ex 3 completed

--- S00188844_ParaicMcDonagh/Phone.cs
+++ S00188844_ParaicMcDonagh/Phone.cs
@@ -8,11 +8,19 @@ namespace S00188844_ParaicMcDonagh
 {
     public class Phone
     {
-        public int Price { get; set; }
+        
+        public double Price { get; set; }
         public string Name { get; set; }
         public string Operating_System { get; set; }
         public string OS_Image { get; set; }
         public string Phone_Image { get; set; }
 
+        //Ex3
+        public void IncreasePrice(double StartingPrice, double PercentageValue)
+        {
+            Price = (StartingPrice * PercentageValue) + StartingPrice;
+        }
     }
+
 }
+
