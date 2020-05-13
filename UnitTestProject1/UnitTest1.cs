4ef0cf2d44b3102c651a418ab16e4ac8b9f0e99f
Paraic Mc Donagh <S00188844@mail.itsligo.ie>
2020-05-13 11:39:02 +0100

    ex 4 done

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using S00188844_ParaicMcDonagh;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IncreasePrice()
+        {
+            //Ex4
+            //Arrange
+            Phone phone = new Phone();
+            double expectedPrice = 500;
+
+            //Act
+            phone.IncreasePrice(500, 10);
+
+            //Assert
+            Assert.AreEqual(expectedPrice, phone.Price);
+
+        }
+    }
+}
