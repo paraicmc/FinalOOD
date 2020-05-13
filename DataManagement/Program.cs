b0d40f41536d0c9ea408dfad46103489c298467e
Paraic Mc Donagh <S00188844@mail.itsligo.ie>
2020-05-13 12:48:42 +0100

    6 nearly completed

--- DataManagement/Program.cs
+++ DataManagement/Program.cs
@@ -18,6 +18,14 @@ namespace DataManagement
                 Phone p1 = new Phone("Samsung S20", 500, "Android", "/images/android.png", "/images/s20.jpg");
                 Phone p2 = new Phone("iPhone 11", 600, "IOS", "/images/apple.png", "/images/iphone11.jpg");
 
+                db.phone.Add(p1);
+                db.phone.Add(p2);
+
+                Console.WriteLine("Add to Database");
+
+                db.SaveChanges();
+
+                Console.WriteLine("Saved to database");
             }
         }
     }
